68bb0d00b07954fa07816ebd41087ac2f0a15a7d
Zac Barnard <barnard.zac@gmail.com>
2018-03-01 22:33:55 -0600

    refactors Reporrt and specs, updates README.md

--- lib/report.rb
+++ lib/report.rb
@@ -13,13 +13,28 @@ class Report
       new_driver(*split_line)
     elsif split_line.first == 'Trip'
       split_line.shift
-      drivers[split_line.first][:driver].record_trip(*split_line)
+      drivers[split_line.first].record_trip(*split_line)
     end
   end
 
+  def generate_report
+    get_data
+    sort_drivers
+    create_output
+  end
+
+  def drivers
+    @drivers ||= {}
+  end
+
+  def driver_data
+    @driver_data ||= []
+  end
+
+  private
   def get_data
     drivers.keys.each do |key|
-      result = drivers[key][:driver].report_stats(TOO_SLOW, TOO_FAST)
+      result = drivers[key].report_stats(TOO_SLOW, TOO_FAST)
       driver_data << result
     end
   end
@@ -29,29 +44,17 @@ class Report
     driver_data.reverse!
   end
 
-  def generate_report
-    formatted_output = ""
-    get_data
-    sort_drivers
+  def create_output
+    output = ""
     driver_data.each do |data|
-        formatted_output << "#{data[:name]}: #{data[:distance]} miles"
-      if data[:distance] != 0
-        formatted_output << " @ #{data[:mph]} mph"
-      end
-        formatted_output << "\n"
+      output << "#{data[:name]}: #{data[:distance]} miles"
+      data[:distance] != 0 ? output << " @ #{data[:mph]} mph" : nil
+      output << "\n"
     end
-     puts formatted_output
+     puts output
   end
 
   def new_driver(name)
-    drivers[name] = { driver: Driver.new(name) }
-  end
-
-  def drivers
-    @drivers ||= {}
-  end
-
-  def driver_data
-    @driver_data ||= []
+    drivers[name] =  Driver.new(name)
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -6,54 +6,54 @@ RSpec.describe Report do
 
 
   it "creates a list of Drivers" do
-    report.new_driver('David')
+    report.send(:new_driver, 'David')
 
-    expect(report.drivers['David'][:driver]).to be_an_instance_of(Driver)
+    expect(report.drivers['David']).to be_an_instance_of(Driver)
   end
 
   it "passes the trip information to the correct Driver" do
-    report.new_driver('Bob')
-    report.new_driver('Marlena')
+    report.send(:new_driver, 'Bob')
+    report.send(:new_driver, 'Marlena')
 
     expect{report.interpret('Trip Bob 11:00 11:30 17.5')}
-    .to change{report.drivers['Bob'][:driver].send(:trips).count}.by(1)
+    .to change{report.drivers['Bob'].send(:trips).count}.by(1)
 
     expect{report.interpret('Trip Bob 11:00 11:30 17.5')}
-    .not_to change{report.drivers['Marlena'][:driver].send(:trips).count}
+    .not_to change{report.drivers['Marlena'].send(:trips).count}
   end
 
   it "gets data from each of its Drivers" do
-    report.new_driver('Mike')
-    report.new_driver('Pedro')
-    allow(report.drivers['Mike'][:driver]).to receive(:report_stats)
-    allow(report.drivers['Pedro'][:driver]).to receive(:report_stats)
+    report.send(:new_driver, 'Mike')
+    report.send(:new_driver, 'Pedro')
+    allow(report.drivers['Mike']).to receive(:report_stats)
+    allow(report.drivers['Pedro']).to receive(:report_stats)
 
-    report.get_data
+    report.send(:get_data)
 
-    expect(report.drivers['Mike'][:driver]).to have_received(:report_stats).with(5, 100)
-    expect(report.drivers['Pedro'][:driver]).to have_received(:report_stats).with(5, 100)
+    expect(report.drivers['Mike']).to have_received(:report_stats).with(5, 100)
+    expect(report.drivers['Pedro']).to have_received(:report_stats).with(5, 100)
   end
 
   it "sorts the data by miles driven" do
-    report.new_driver('Jimmy')
-    report.new_driver('Clara')
-    report.drivers['Jimmy'][:driver].record_trip('Jimmy', '10:00', '11:00', '35')
-    report.drivers['Clara'][:driver].record_trip('Clara', '12:00', '14:00', '90')
+    report.send(:new_driver, 'Jimmy')
+    report.send(:new_driver, 'Clara')
+    report.drivers['Jimmy'].record_trip('Jimmy', '10:00', '11:00', '35')
+    report.drivers['Clara'].record_trip('Clara', '12:00', '14:00', '90')
 
-    report.get_data
-    report.sort_drivers
+    report.send(:get_data)
+    report.send(:sort_drivers)
 
     expect(report.driver_data[0][:name]).to eq('Clara')
     expect(report.driver_data[1][:name]).to eq('Jimmy')
   end
 
   it "generates and prints a report" do
-    report.new_driver('Christoph')
-    report.new_driver('Cindy')
-    report.new_driver('Tinisha')
-    report.drivers['Cindy'][:driver].record_trip('Cindy', '10:00', '11:00', '35')
-    report.drivers['Cindy'][:driver].record_trip('Cindy', '12:00', '14:00', '90')
-    report.drivers['Tinisha'][:driver].record_trip('Tinisha', '11:00', '11:30', '17.5')
+    report.send(:new_driver, 'Christoph')
+    report.send(:new_driver, 'Cindy')
+    report.send(:new_driver, 'Tinisha')
+    report.drivers['Cindy'].record_trip('Cindy', '10:00', '11:00', '35')
+    report.drivers['Cindy'].record_trip('Cindy', '12:00', '14:00', '90')
+    report.drivers['Tinisha'].record_trip('Tinisha', '11:00', '11:30', '17.5')
 
     expect{report.generate_report}.to output(generated_report).to_stdout
   end
